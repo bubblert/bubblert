428227db8eeb97f85956b13cd71e8652b308cd48
Jacek Bylina <jacek.bylina@gmail.com>
2017-09-16 01:36:54 +0200

    fixes

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ from app import socketio, app, celery
 
 if __name__ == '__main__':
     app_thread = threading.Thread(target=lambda: socketio.run(app, port=5000), daemon=True)
-    celery_thread = threading.Thread(target=lambda: celery.start(argv=['celery', 'worker', '-l', 'info']), daemon=True)
+    # celery_thread = threading.Thread(target=lambda: celery.start(argv=['celery', 'worker', '-l', 'info']), daemon=True)
+    # celery_thread.start()
 
-    celery_thread.start()
     app_thread.start()
 
     app_thread.join()
-    celery_thread.join()
+    # celery_thread.join()
 
