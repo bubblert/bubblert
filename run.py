30921f095c38597ea366641446bcbe231f0edae8
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-09-16 08:30:41 +0200

    Fix host

--- run.py
+++ run.py
@@ -94,7 +94,7 @@ def runserver():
     scheduler.init_app(app)
     scheduler.start()
 
-    socketio.run(app, host='localhost', port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000)
 
 
 if __name__ == '__main__':
