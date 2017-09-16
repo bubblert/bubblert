6dfd46e3802490b0e7ae9527348fd8b093e3cf0e
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-09-16 02:34:11 +0200

    Fix lint errors, make server work

--- run.py
+++ run.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
-from app import socketio, app
+import socketio
+import eventlet
+import eventlet.wsgi
+from flask import Flask, render_template
+
+sio = socketio.Server()
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Serve the client-side application."""
+    return render_template('index.html')
+
+
+@sio.on('connect', namespace='/chat')
+def connect(sid, environ):
+    print("connect ", sid)
+
+
+@sio.on('chat message', namespace='/chat')
+def message(sid, data):
+    print("message ", data)
+    sio.emit('reply', room=sid)
+
+
+@sio.on('disconnect', namespace='/chat')
+def disconnect(sid):
+    print('disconnect ', sid)
 
 
 if __name__ == '__main__':
-    socketio.run(app, port=5000)
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
